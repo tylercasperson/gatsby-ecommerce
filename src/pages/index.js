7ed3171bfbfedcb876b5a66ed2573f4e6555993d
Tyler Casperson <tylercasperson@gmail.com>
2020-09-01 21:59:28 -0600

    created an item listing that can be purchased

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,36 +1,41 @@
 import React from 'react';
-import { Link } from 'gatsby';
 
 import Layout from '../components/layout';
-import Image from '../components/image';
 import SEO from '../components/seo';
 
 const IndexPage = () => {
   const stripe = window.Stripe(process.env.stripe_api_key);
 
-  stripe.redirectToCheckout({
-    lineItems: [
-      {
-        price: 'price_1HMkrlD9PqAGwk1KHi3XFc4A',
-        quantity: 1,
-      },
-    ],
-    mode: 'payment',
-    successUrl: 'http://localhost:8000/',
-    cancelUrl: 'http://localhost:8000/',
-  });
+  const placeOrder = (item) => {
+    stripe.redirectToCheckout({
+      lineItems: [
+        {
+          price: item,
+          quantity: 1,
+        },
+      ],
+      mode: 'payment',
+      successUrl: 'http://localhost:8000/',
+      cancelUrl: 'http://localhost:8000/',
+    });
+  };
 
   return (
     <Layout>
       <SEO title='Home' />
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <p>Now go build something great.</p>
-      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-        <Image />
+      <h1>Gatsby Ecommerce Store</h1>
+      <div>
+        <article>
+          <img
+            src='https://picsum.photos/200/300'
+            alt='gatsbyEcommerceTshirt'
+          />
+          <h3>Gatsby Ecommerce T-Shirt</h3>
+          <button onClick={() => placeOrder('price_1HMkrlD9PqAGwk1KHi3XFc4A')}>
+            Buy Me
+          </button>
+        </article>
       </div>
-      <Link to='/page-2/'>Go to page 2</Link> <br />
-      <Link to='/using-typescript/'>Go to "Using TypeScript"</Link>
     </Layout>
   );
 };
